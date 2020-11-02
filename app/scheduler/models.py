54144bc9bcb02233aa4595064a5c26b156ad69d3
abulojoshua1 <abulojoshua1@gmail.com>
2020-11-02 19:09:36 +0300

    remove commented out code

--- app/scheduler/models.py
+++ app/scheduler/models.py
@@ -55,34 +55,3 @@ class Task(models.Model):
     @property
     def status_icon(self):
         return status_icon_mapper.get(self.status, u"")
-
-    @property
-    def task_log(self):
-        if os.path.exists(self.logfile):
-            task_log = Path(self.logfile).read_text()
-            if task_log == u"(True,)\n":
-                return u"Task completed successfully"
-            return task_log
-
-
-# class Process(models.Model):
-#     name = models.CharField(max_length=250, blank=False)
-#     algorithm = models.CharField(max_length=50, blank=False)
-
-#     def __str__(self):
-#         return self.name
-
-
-# class ProcessHistory(models.Model):
-#     process = models.ForeignKey(Process, on_delete=models.CASCADE)
-#     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-
-#     def __str__(self):
-#         return f"process_id={self.process.pk}:task_id={self.task.pk}"
-
-#     def run_process_algorith(self):
-#         analysis_cursor = connection.cursor()
-#         with analysis_cursor as cursor:
-#             cursor.callproc(f"dbiait_analysis.{self.process.algorithm}")
-#             result = cursor.fetchone()
-#         return result
