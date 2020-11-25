310a00face4ba72fcfa43145cdfe5255786564fe
mattiagiupponi <mattia.giupponi@gmail.com>
2020-11-25 10:55:46 +0100

    Resolves #104: rollback analysis_router.py

--- app/scheduler/analysis_router.py
+++ app/scheduler/analysis_router.py
@@ -9,9 +9,9 @@ class AnalysisRouter:
     db_key = 'analysis'
 
     def _check_name(self, name):
-        if name.startswith("auth_") or name.startswith("django_") or name.startswith("scheduler_"):
-            return False
-        return True
+        if name.lower() == "all_domains":
+            return True
+        return False
 
     def db_for_read(self, model, **hints):
         """
