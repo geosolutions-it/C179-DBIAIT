69d3627dca5b723e758fbc3367ca4b9ac1b9683d
Mattia Giupponi <mattia.giupponi@gmail.com>
2020-12-03 16:34:47 +0100

    Resolves #148: Documentation added and add manage of dependencies with other fields

--- app/scheduler/tasks/export_definitions/validations.py
+++ app/scheduler/tasks/export_definitions/validations.py
@@ -1,6 +1,9 @@
+import re
+
 import schema
 from .exceptions import ExportConfigError
 from app.scheduler.utils import COMPARISON_OPERATORS_MAPPING
+from typing import Dict
 
 SUPPORTED_VALIDATIONS = ["IF"]
 
@@ -22,31 +25,92 @@ class BaseValidation:
 
 
 class IfValidation(BaseValidation):
+    """
+        If validation, will check if a specific value match the condition.
+        The conditions can be multiple in AND or OR (or both means that both conditions must be True)
+        The validation also accept templated field that must be added into a specific key called "lookup"
+        with "{ column_alias }" as alias.
+        Simple example with AND:
+        {
+            "field": "foo_field",
+            "cond": {
+                "and": [
+                    {"operator": ">", "value": 2},
+                    {"operator": "<", "value": 10},
+                ]
+            },
+        }
+        return True if the value of "foo_field" is greater than 2 and lower than 10
+
+        Simple example with OR:
+        {
+            "field": "foo_field",
+            "cond": {
+                "or": [
+                    {"operator": ">", "value": 2},
+                    {"operator": "<", "value": 10},
+                ]
+            },
+        }
+        return True if the value of "foo_field" is greater than 2 OR lower than 10
+
+        Simple example with AND, OR and templated field:
+        {
+            "field": "foo_field",
+            "cond": {
+                "and": [
+                    {"lookup": "{ bar_field }", "operator": ">", "value": 2},
+                    {"operator": "<", "value": 10},
+                ],
+                "or": [
+                    {"lookup": ">", "value": 3},
+                    {"operator": "<", "value": 25},
+                ]
+            },
+        }
+        return True if
+        - the value of "bar_field" is greater than 2
+        - if the value of bar_field is lower than 10
+        OR if the value of "foo_field" is greater than 3 or lower than 25"
+    """
+
     schema = schema.Schema(
         {
+            "field": str,
             "cond":
-                schema.Or(
-                    {str: [{"operator": str, "value": object}]}
+                schema.And(
+                    {str: [{schema.Optional('lookup'): str, "operator": str, "value": object}]}
                 )
         }
     )
+    re_pattern = re.compile('{\W*(\w+)\W*}')
 
-    def validate(self, value):
+    def validate(self, row: Dict):
         conditions = self.args["cond"]
         and_conditions = conditions.get("and", [])
         or_conditions = conditions.get("or", [])
-        and_result = list(self._validate_list(and_conditions, value))
-        or_result = list(self._validate_list(or_conditions, value))
-        if len(or_result) > 0:
-            return any(or_result + and_result)
-        elif len(and_result) > 0 and len(or_result) == 0:
-            return all(and_result + or_result)
 
-    @staticmethod
-    def _validate_list(conditions, value):
+        and_result = list(self._validate_list(and_conditions, row))
+        or_result = list(self._validate_list(or_conditions, row))
+
+        if len(and_result) > 0 and len(or_result) > 0:
+            return all([and_result, any(or_result)])
+        elif len(or_result) > 0:
+            return all([any(or_result), all(and_result)])
+        elif len(and_result) > 0 and len(or_result) == 0:
+            return all(and_result)
+
+    def _validate_list(self, conditions, row):
+        field_value = row.get(self.args["field"], None)
         for cond in conditions:
+            if "lookup" in cond:
+                lookup_field = re.match(self.re_pattern, cond['lookup'])
+
+                if lookup_field is not None:
+                    field_value = row.get(lookup_field.group(1), None)
+
             operator = COMPARISON_OPERATORS_MAPPING.get(cond["operator"], None)
-            yield operator(value, cond["value"])
+            yield operator(field_value, cond["value"])
 
 
 class ValidationFactory:
--- app/tests/tasks/test_validations.py
+++ app/tests/tasks/test_validations.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest import skip
 
 from django.test import SimpleTestCase
 
@@ -9,53 +8,111 @@ from app.scheduler.tasks.export_definitions.validations import ValidationFactory
 class ValidationTestCase(SimpleTestCase):
     def setUp(self) -> None:
         self.validate = ValidationFactory
+        self.field = {"foo_field": 1, "bar_field": 12}
 
-    def test_given_transformation_name_IF_AND_with_true_cond_should_return_the_expected_output(self):
+    def test_given_transformation_name_IF_AND_with_true_cond_should_return_the_expected_output(
+        self,
+    ):
         condition_schema = {
-            "cond": {
-                "and": [
-                    {"operator": "<", "value": "2"}
-                ]
-            }
+            "field": "foo_field",
+            "cond": {"and": [{"operator": "<", "value": 2}]},
         }
-        actual = self.validate.from_name("IF", condition_schema).validate("1")
+        actual = self.validate.from_name("IF", condition_schema).validate(self.field)
         self.assertTrue(actual)
 
-    def test_given_transformation_name_IF_AND_with_false_cond_should_return_the_expected_output(self):
+    def test_given_transformation_name_IF_AND_with_true_cond_and_not_matching_regex_should_return_the_expected_output(
+        self,
+    ):
         condition_schema = {
+            "field": "foo_field",
+            "cond": {"and": [{"lookup": "{bar_field}", "operator": "<", "value": 2}]},
+        }
+        actual = self.validate.from_name("IF", condition_schema).validate(self.field)
+        self.assertFalse(actual)
+
+    def test_given_transformation_name_IF_AND_with_false_cond_should_return_the_expected_output(
+        self,
+    ):
+        condition_schema = {
+            "field": "foo_field",
             "cond": {
                 "and": [
-                    {"operator": "<", "value": "2"},
-                    {"operator": ">", "value": "10"},
+                    {"operator": "<", "value": 2},
+                    {"operator": ">", "value": 10},
                 ]
-            }
+            },
         }
-        actual = self.validate.from_name("IF", condition_schema).validate("1")
+        actual = self.validate.from_name("IF", condition_schema).validate(self.field)
         self.assertFalse(actual)
 
-    def test_given_transformation_name_IF_OR_with_true_cond_should_return_the_expected_output(self):
+    def test_given_transformation_name_IF_OR_with_true_cond_should_return_the_expected_output(
+        self,
+    ):
         condition_schema = {
+            "field": "foo_field",
             "cond": {
                 "or": [
-                    {"operator": "<", "value": "2"},
-                    {"operator": ">", "value": "0"},
+                    {"operator": "<", "value": 2},
+                    {"operator": ">", "value": 0},
                 ]
-            }
+            },
         }
-        actual = self.validate.from_name("IF", condition_schema).validate("1")
+        actual = self.validate.from_name("IF", condition_schema).validate(self.field)
         self.assertTrue(actual)
 
-    def test_given_transformation_name_IF_OR_with_false_cond_should_return_the_expected_output(self):
+    def test_given_transformation_name_IF_OR_with_false_cond_should_return_the_expected_output(
+        self,
+    ):
         condition_schema = {
+            "field": "foo_field",
             "cond": {
                 "or": [
-                    {"operator": ">", "value": "2"},
-                    {"operator": ">", "value": "10"},
+                    {"operator": ">", "value": 2},
+                    {"operator": ">", "value": 10},
                 ]
-            }
+            },
         }
-        actual = self.validate.from_name("IF", condition_schema).validate("1")
+        actual = self.validate.from_name("IF", condition_schema).validate(self.field)
         self.assertFalse(actual)
+
+    def test_given_transformation_name_IF_AND_OR_with_false_cond_should_return_the_expected_output(
+        self,
+    ):
+        condition_schema = {
+            "field": "foo_field",
+            "cond": {
+                "or": [
+                    {"operator": ">", "value": 2},
+                    {"operator": ">", "value": 10},
+                ],
+                "and": [
+                    {"operator": "<", "value": 2},
+                    {"operator": ">", "value": 10},
+                ],
+            },
+        }
+        actual = self.validate.from_name("IF", condition_schema).validate(self.field)
+        self.assertFalse(actual)
+
+    def test_given_transformation_name_IF_AND_OR_with_true_cond_should_return_the_expected_output(
+        self,
+    ):
+        condition_schema = {
+            "field": "foo_field",
+            "cond": {
+                "or": [
+                    {"operator": "=", "value": 1},
+                    {"operator": "<", "value": 100},
+                ],
+                "and": [
+                    {"operator": "<", "value": 10},
+                    {"operator": ">", "value": 0},
+                ],
+            },
+        }
+        self.field = {"foo_field": 5, "bar_field": 12}
+        actual = self.validate.from_name("IF", condition_schema).validate(self.field)
+        self.assertTrue(actual)
 
 
 if __name__ == "__main__":
