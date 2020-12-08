930b98e3f8ebb65caaee7d1aa2d53d0b91388d9a
Christian R. Picone <christian.picone@geo-solutions.it>
2020-12-08 11:08:27 +0100

    add functions is_null and to_bit

--- app/scheduler/tasks/export_definitions/config_scraper/sql_functions.py
+++ app/scheduler/tasks/export_definitions/config_scraper/sql_functions.py
@@ -1,10 +1,17 @@
 from pypika import CustomFunction
 
+is_null = CustomFunction("IS_NULL", ["v_value"])
 gb_x = CustomFunction("GB_X", ["v_geom"])
 gb_y = CustomFunction("GB_Y", ["v_geom"])
 st_transform_4326 = CustomFunction("ST_TRANSFORM_4326", ["v_geom"])
+to_bit = CustomFunction("TO_BIT", ["v_value"])
 
-SQL_FUNCTION_MAPPING = {"GB_X": gb_x, "GB_Y": gb_y, "ST_TRANSFORM_4326": st_transform_4326}
+SQL_FUNCTION_MAPPING = {
+    "IS_NULL": is_null,
+    "GB_X": gb_x, "GB_Y": gb_y,
+    "ST_TRANSFORM_4326": st_transform_4326,
+    "TO_BIT": to_bit
+}
 
 SUPPORTED_SQL_FUNCTIONS = list(SQL_FUNCTION_MAPPING.keys())
 
