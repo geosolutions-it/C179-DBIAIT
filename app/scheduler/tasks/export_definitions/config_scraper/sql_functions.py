5bbd6b78e3c20155c011dd3ddf92b567f5576267
Christian R. Picone <christian.picone@geo-solutions.it>
2020-12-19 17:46:54 +0100

    fix some error in the export #188

--- app/scheduler/tasks/export_definitions/config_scraper/sql_functions.py
+++ app/scheduler/tasks/export_definitions/config_scraper/sql_functions.py
@@ -5,6 +5,9 @@ gb_x = CustomFunction("GB_X", ["v_geom"])
 gb_y = CustomFunction("GB_Y", ["v_geom"])
 st_x = CustomFunction("ST_X", ["v_geom"])
 st_y = CustomFunction("ST_Y", ["v_geom"])
+st_round_x = CustomFunction("ST_ROUND_X", ["v_geom"])
+st_round_y = CustomFunction("ST_ROUND_Y", ["v_geom"])
+
 st_transform_4326 = CustomFunction("ST_TRANSFORM_4326", ["v_geom"])
 to_bit = CustomFunction("TO_BIT", ["v_value"])
 
@@ -12,6 +15,7 @@ SQL_FUNCTION_MAPPING = {
     "IS_NULL": is_null,
     "GB_X": gb_x, "GB_Y": gb_y,
     "ST_X": st_x, "ST_Y": st_y,
+    "ST_ROUND_X": st_round_x, "ST_ROUND_Y": st_round_y,
     "ST_TRANSFORM_4326": st_transform_4326,
     "TO_BIT": to_bit
 }
