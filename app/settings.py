f9b2ca2007c6c06e1dfc068822fa144ba45a5fec
Mattia Giupponi <mattia.giupponi@gmail.com>
2021-01-08 17:12:08 +0100

    Resolves #203: increase version number

--- app/settings.py
+++ app/settings.py
@@ -17,7 +17,7 @@ import ldap
 from app.utils import TemplateWithDefaults
 from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
 
-APP_VERSION = '1.2.0 (22/12/2020)'
+APP_VERSION = '1.3.0 (08/01/2021)'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
