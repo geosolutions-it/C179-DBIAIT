1e776ce76279c2d55926c5c837ba80f08b367d00
Christian R. Picone <christian.picone@geo-solutions.it>
2021-02-13 17:01:29 +0100

    bug fix and new data in domains.csv version 1.7.0

--- app/settings.py
+++ app/settings.py
@@ -19,7 +19,7 @@ import ldap
 from app.utils import TemplateWithDefaults
 from django_auth_ldap.config import LDAPSearch, GroupOfNamesType
 
-APP_VERSION = '1.6.0 (06/02/2021)'
+APP_VERSION = '1.7.0 (13/02/2021)'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
