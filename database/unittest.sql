0eaba270d7a20c7dcb186c927ae9a7c3950f8df1
Christian R. Picone <christian.picone@geo-solutions.it>
2021-04-28 13:04:37 +0200

    update UT for codice accorp

--- database/unittest.sql
+++ database/unittest.sql
@@ -1688,13 +1688,12 @@ DECLARE
   v_count          BIGINT:=0;
   v_expected       BIGINT:=1;
   v_denom          VARCHAR;
-  v_expected_denom VARCHAR := 'CSO_S. ROMOLO';
 BEGIN
     select count(0) INTO v_count from dbiait_analysis.support_codice_capt_accorp where idgis='PAACAP00000000012960' ;
     perform test_assertTrue('populate_codice_capt_accorp (PAACAP00000000012960):, expected ' || v_expected || ' but found ' || v_count, v_count = v_expected );
 
-    select denom INTO v_denom from dbiait_analysis.support_codice_capt_accorp where idgis='PAACAP00000000012960' ;
-    perform test_assertTrue('populate_codice_capt_accorp (PAACAP00000000012960):, expected ' || v_expected_denom || ' but found ' || v_denom, v_denom = v_expected_denom );
+    select COALESCE(denom, '?') INTO v_denom from dbiait_analysis.support_codice_capt_accorp where idgis='PAACAP00000000012960' ;
+    perform test_assertTrue('populate_codice_capt_accorp (PAACAP00000000012960):, expected <NULL> but found ' || v_denom, v_denom = '?' );
 END;
 $$  LANGUAGE plpgsql SECURITY DEFINER SET search_path = public,pgunit;
 --------------------------------------------------------------------------------------------
@@ -1703,13 +1702,12 @@ DECLARE
   v_count          BIGINT:=0;
   v_expected       BIGINT:=1;
   v_denom          VARCHAR;
-  v_expected_denom VARCHAR := 'CPO_NAVANELLA 5';
 BEGIN
     select count(0) INTO v_count from dbiait_analysis.support_codice_capt_accorp where idgis='PAACAP00000000011433' ;
     perform test_assertTrue('populate_codice_capt_accorp (PAACAP00000000011433):, expected ' || v_expected || ' but found ' || v_count, v_count = v_expected );
 
-    select denom INTO v_denom from dbiait_analysis.support_codice_capt_accorp where idgis='PAACAP00000000011433' ;
-    perform test_assertTrue('populate_codice_capt_accorp (PAACAP00000000011433):, expected ' || v_expected_denom || ' but found ' || v_denom, v_denom = v_expected_denom );
+    select COALESCE(denom, '?') INTO v_denom from dbiait_analysis.support_codice_capt_accorp where idgis='PAACAP00000000011433' ;
+    perform test_assertTrue('populate_codice_capt_accorp (PAACAP00000000011433):, expected <NULL> but found ' || v_denom, v_denom = '?' );
 END;
 $$  LANGUAGE plpgsql SECURITY DEFINER SET search_path = public,pgunit;
 --------------------------------------------------------------------------------------------
