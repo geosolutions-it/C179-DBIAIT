23650cd5b5647157bc4f6ec7c11a398fa3208c1d
Mattia <mattia.giupponi@gmail.com>
2025-09-22 12:37:19 +0200

    Fix command in ogr2ogr

--- app/scheduler/tasks/import_definitions/import_gpkg.py
+++ app/scheduler/tasks/import_definitions/import_gpkg.py
@@ -120,12 +120,15 @@ class GpkgImportDefinition(BaseImportDefinition):
         if gtype != 3:
             options += '-nlt PROMOTE_TO_MULTI'
         commands = [ogr_exe, options]
+        print(commands)
         return commands
 
     def execute_command(self, commands, feedback):
         from subprocess import PIPE, Popen
         process = Popen(" ".join(commands), stdout=PIPE, stderr=PIPE, shell=True)
         stdout, stderr = process.communicate()
+        print(stdout)
+        print(stderr)
         if (
             stderr is not None
             and stderr != b""
